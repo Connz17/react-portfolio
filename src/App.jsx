f79ef3cb4961f8e9811c2956d3a51f63c57e2566
Connel Nwaodor <connel_n@hotmail.com>
2023-02-10 17:14:50 +0000

    Project folders created

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.scss';
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+
+
+function App() {
+  return (
+  <Router >
+    <div className="App">
+      <Routes>
+        <Route>
+
+        </Route>
+      </Routes>
+    </div>
+  </Router>
+  );
+}
+
+export default App;
