9a9c49481c7fde475a7e3e30bc6f0710025f471a
Connel Nwaodor <connel_n@hotmail.com>
2024-08-17 00:41:44 +0100

    Project data template complete

---
+++ src/data/ProjectData.jsx
@@ -0,0 +1,98 @@
+
+
+
+export const ProjectData = [
+    {
+        frontend: [
+            {
+                project: "BrewDog beer punk api",
+                title: "#",
+                thumbnail: "",
+                alt:"",
+                link:"",
+            },
+            {
+                project: "Morse Code Translator",
+                title: "#",
+                thumbnail: "",
+                alt:"",
+                link:"",
+            },
+            {
+                project: "Weather App",
+                title: "#",
+                thumbnail: "",
+                alt:"",
+                link:"",
+            },
+            {
+                project: "Javascript Browser Game",
+                title: "#",
+                thumbnail: "",
+                alt:"",
+                link:"",
+            },
+            {
+                project: "JavaScript Calculator",
+                title: "#",
+                thumbnail: "",
+                alt:"",
+                link:"",
+            },
+            {
+                project: "Ticket Tracker",
+                title: "#",
+                thumbnail: "",
+                alt:"",
+                link:"",
+            },
+            {
+                project: "Property App",
+                title: "#",
+                thumbnail: "",
+                alt:"",
+                link:"",
+            }
+        ], 
+        backend: [
+        {
+            project: "Java Calculator",
+            title: "#",
+            thumbnail: "",
+            alt:"",
+            link:"",
+        },
+        {
+            project: "Java Hangman",
+            title: "#",
+            thumbnail: "",
+            alt:"",
+            link:"",
+        },
+        {
+            project: "Java Card Game",
+            title: "#",
+            thumbnail: "",
+            alt:"",
+            link:"",
+        },
+        {
+            project: "Java Library System",
+            title: "#",
+            thumbnail: "",
+            alt:"",
+            link:"",
+        }
+        ],
+        fullstack:
+        [
+            {
+                project: "Dark Souls",
+                title: "#",
+                thumbnail: "",
+                alt:"",
+                link:"",
+            }
+        ] 
+    }
+]
