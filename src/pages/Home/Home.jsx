f0648364daa9dda0fff71baf2fb9334c6844432e
Connel Nwaodor <connel_n@hotmail.com>
2024-10-12 16:41:24 +0100

    edited home page typewriter

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import Nav from "../../containers/Nav/Nav"
 import "./Home.scss"
-import pageImg from "../../images/svgs/code-circle-svgrepo-com.svg";
 
 
 
@@ -10,18 +9,41 @@ const Home = () => {
     <>
       <Nav />
       <div className="home">
+        <main>
           <div className="typewriter">
             <div className="titleContainer" id="containerOne">
-              <h2 className="titleMessage">Welcome, I'm Connel a software developer.</h2>
+              <h2>Welcome, I'm Connel a software developer.</h2>
             </div>
+          </div>
+            <br />
+          <div className="typewriter">
+            <div className="tcOne">
+              <h2>Please take a look through my portfolio and see some of my projects and tech stack!</h2>
+            </div>
+          </div>
+          <div className="typewriter">
             <div className="titleContainer" id="containerTwo">
               <h2 className="titleMessage">Connel, software developer.</h2>
             </div>
+          </div>
+          <br />
+          <div className="typewriter">
+            <div className="tcTwo">
+              <h2>Take a look through my projects and tech stack!</h2>
+            </div>
+          </div>
+          <div className="typewriter">
             <div className="titleContainer" id="containerThree">
               <h2 className="titleMessage">Welcome</h2>
-            </div>
-            <img className="homeImg" src={pageImg} alt="code circle" />
+            </div>  
           </div>
+          <br /> 
+          <div className="typewriter">
+            <div className="tcThree">
+              <h2>Take a look through my portfolio!</h2>
+            </div>
+          </div>
+        </main>
       </div>
     </>
   )
