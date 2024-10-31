bf7fea35939964199a4cf216b8ac8e7c5bcadd4a
Connel Nwaodor <connel_n@hotmail.com>
2024-10-31 17:54:00 +0000

    Brew dod and JS calculator project information written

--- src/data/ProjectData.jsx
+++ src/data/ProjectData.jsx
@@ -49,17 +49,17 @@ frontend: [
         project: "JavaScript Calculator",
         title: "JavaScript Calculator",
         'Project Information': 
-`### Overview       
-This project is a **vanilla JavaScript calculator**. 
-The brief for this project was to make a web-based calculator using vanilla JavaScript. 
-The calculator needed to handle user inputs and give the correct output based on the numbers entered and the operation selected. 
-The advanced goal was to allow the calculator to perform chain calculations.
-#### Key parts of the code
+`### Project Overview       
+This web-based calculator, built with vanilla JavaScript, allows users to perform basic arithmetic operations seamlessly. Designed to provide an intuitive experience, the calculator handles user inputs with accuracy, displaying real-time updates on the screen. It also supports chain calculations, letting users perform continuous operations without needing to clear the screen after each result.
+
+This project highlights my skills in JavaScript, DOM manipulation, and user interface design, focusing on functional, interactive web elements.
+
+#### Key elements of the code
 Two key elements in programming this calculator are:
-- The switch case that runs the correct mathematical operation based on user selection.
+- Utilising a switch case structure to execute the correct mathematical operation based on user selection, ensuring responsive and accurate calculations.
 - The output screen, which displays the current number selection in the bottom half of the screen and updates to show the operation selected, moving the number to the top half of the screen and updating the bottom with the second number inputted. 
-#### Key feature
-A cool feature I implemented is keyboard functionality, allowing users to input numbers and select operations using their keyboard.
+#### Key features
+A cool feature I implemented is keyboard input support, users can input numbers and select operations directly from their keyboard for a more flexible experience.
 `, 
         'Languages Used':["HTML", "CSS", "JavaScript"],
         'Future Scope': 
@@ -79,23 +79,27 @@ A cool feature I implemented is keyboard functionality, allowing users to input 
         project: "BrewDog beer punk api",
         title: "Brew Dog API",
         'Project Information':
-`### Overview       
-This was a **xxxx** project. 
-The brief for this project was to make a xxxxxxx. 
-The advanced goal was to allow the xxxxxxxxxxx.
-#### Key parts of the code
-key elements in programming this project was:
-- a
-- b
-#### Key feature
+`### Project Overview       
+This React-based web application was built to showcase the entire range of BrewDog beers using a public API. The app displays all available beers, with additional features such as dynamic filtering, separate pages for curated beer categories, and individual beer detail pages. Each component was designed to provide an intuitive and seamless user experience, enabling users to explore the BrewDog catalog with ease.
 
+This project demonstrates my ability to integrate complex API interactions, optimize data handling, and build user-friendly interfaces using React.
+
+#### Key elements of the code
+key elements in programming this project was:
+- Utilising the useEffect hook to manage API requests efficiently, minimizing unnecessary fetches and improving overall performance. 
+- Utilising React Router for seamless navigation between pages, with dedicated sections for specific beer categories. 
+- Employing Employed useParams to dynamically render individual beer pages, showcasing comprehensive information about each beer a user selects.
+#### Key features
+2 cool features I implemented in this project were
+1) Implemented a looping API request function to overcome data limitations, fetching all 300+ beers across multiple requests rather than the default 80-per-call limit.
+2)  Creating a versatile search tool, allowing users to filter results based on various beer characteristics, including name and specific attributes.
 `,
         'Languages Used':["React", "SCSS"],
         'Future Scope':
     `For the next version/iteration of this project, the things I'd like to add are:
-- Implement a xxxxxxxxx.
-- Implement a xxxxxxxx.
-- Implement xxxxxxxxx.
+- Implement User feedback for when no beers are shown.
+- Having all sliding filters on one page and working together.
+- Update the UI.
 `,    thumbnail: beerpunk,
         screenshots:beerScreenshots,
         preview: brewdogPreview,
@@ -107,7 +111,7 @@ key elements in programming this project was:
         project: "Morse Code Translator",
         title: "Morse Code Translator",
         'Project Information':
-        `### Overview       
+        `### Project Overview       
 This was a **xxxx** project. 
 The brief for this project was to make a xxxxxxx. 
 The advanced goal was to allow the xxxxxxxxxxx.
